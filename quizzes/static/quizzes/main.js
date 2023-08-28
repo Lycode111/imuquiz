3ca74d391e355f22787fef6ba25a7ad2900f2917
Renice Chin <renicechin@icloud.com>
2023-08-28 15:26:02 +0800

    changes to quiz javascript, css

--- quizzes/static/quizzes/main.js
+++ quizzes/static/quizzes/main.js
@@ -18,10 +18,10 @@ modalBtns.forEach(modalBtn=> modalBtn.addEventListener('click',()=>{
         <div class="h5 mb-3">Are you sure you want to begin "<b>${name}</b>"?</div>
         <div class="text-muted">
             <ul>
-                <li>difficulty: <b>${difficulty}</b></li>
-                <li>number of questions: <b>${numQuestions}</b></li>
-                <li>score to pass: <b>${scoreToPass}%</b></li>
-                <li>time: <b>${time} min</b></li>
+                <li>Difficulty: <b style="text-transform: uppercase;">${difficulty}</b></li>
+                <li>Number of Questions: <b>${numQuestions}</b></li>
+                <li>Passing Score: <b>${scoreToPass}%</b></li>
+                <li>Time Limit: <b>${time} minute</b></li>
             </ul>
         </div>
     `
