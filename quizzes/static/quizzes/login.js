4e6f2bee2c04b8381b005c450c1d60a4181f3e12
Renice Chin <renicechin@icloud.com>
2023-09-06 11:52:06 +0800

    navbar active

---
+++ quizzes/static/quizzes/login.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $(".nav li").removeClass("active");//this will remove the active class from  
+                                       //previously active menu item 
+    $('#login').addClass('active');
+  });
